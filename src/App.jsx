8e10265378df24a5fe11840adfb347354ad1a55a
Himanshu <hjasaiwal114@gmail.com>
2024-07-03 19:31:34 +0530

    creating components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './compontents/Headers'
+import Hero from './compontents/Hero'
+import Services from './compontents/Services'
+import About from './compontents/About'
+import Counter from './compontents/Counter'
+import Pricing from './compontents/Pricing'
+import Clients from './compontents/Clients'
+import Contact from './compontents/Contact'
+import Footer from './compontents/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Hero />
+    <Services />
+    <About />
+    <Counter />
+    <Pricing />
+    <Clients />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default App;
